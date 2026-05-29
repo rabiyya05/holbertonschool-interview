190e0bac9a922019b55d8a42749adc416cd27163
rabiyya05 <ismayilovarabia5@gmail.com>
2026-05-28 23:53:26 -0400

    test

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,7 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void topple_sandpiles(int grid[3][3], int t_points[9][2]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
