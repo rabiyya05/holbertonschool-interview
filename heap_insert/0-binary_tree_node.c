9a1726d07debc99d25ed1a5d52406884aa41ab6d
rabiyya05 <ismayilovarabia5@gmail.com>
2026-05-28 23:43:50 -0400

    test

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,24 +1,32 @@
+u#include <stdlib.h>
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: int value to put in the new node
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: The value to put in the new node
+ *
+ * Return: A pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = NULL;
+    binary_tree_t *new_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
-		return (NULL);
+    /* 1. Yeni qovşaq üçün yaddaş ayırırıq */
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+    /* 2. Qovşağın daxili dəyişənlərini (məlumatlarını) doldururuq */
+    new_node->n = value;
+    new_node->parent = parent;
+    
+    /* Tapşırığa əsasən yeni yaranan qovşağın başlanğıcda heç bir övladı olmur */
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-	return (new);
-}
+    /* 3. Yaradılmış qovşağın ünvanını geri qaytarırıq */
+    return (new_node);
+}}
